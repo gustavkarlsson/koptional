7521f24d9d24b027c14cd92b694b075316fac26a
StefMa <StefMaDev@outlook.com>
2020-10-09 13:33:25 +0200

    Remove deprecated

--- lib/src/main/kotlin/se/gustavkarlsson/koptional/Creation.kt
+++ lib/src/main/kotlin/se/gustavkarlsson/koptional/Creation.kt
@@ -1,8 +1,5 @@
 package se.gustavkarlsson.koptional
 
-@Deprecated("Useless", ReplaceWith("Absent"), DeprecationLevel.ERROR)
-fun <T : Any> absent(): Optional<T> = Absent
-
 fun <T : Any> optionalOf(value: T?): Optional<T> = if (value == null) Absent else Present(value)
 
 fun <T : Any> T?.toOptional(): Optional<T> = optionalOf(this)
